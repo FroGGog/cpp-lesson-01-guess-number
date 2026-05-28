fcad424c489924506f19f1d79ca6f771ee0e02d9
Danya Bucenin <danyabucenin@gmail.com>
2026-05-28 15:20:25 +0300

    Added perfect solution

--- students/test-student/guess_number.cpp
+++ students/test-student/guess_number.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
-    std::cout << "Hello, world, my solution 1!\n";
+    // Инициализируем генератор случайных чисел
+    // time(0) возвращает текущее время в секундах
+    std::srand(std::time(0));
+    
+    bool playAgain = true;
+    
+    while (playAgain)
+    {
+        int secretNumber = std::rand() % 100 + 1;
+        int guess = 0;
+        int attempts = 0;
+        
+        std::cout << "Я загадал число от 1 до 100. Попробуй угадать!\n" << std::endl;
+        
+        // Основной цикл: крутится пока не угадаем
+        while (guess != secretNumber)
+        {
+            std::cout << "Твоя догадка: ";
+            std::cin >> guess;
+            attempts++;
+            
+            if (guess > secretNumber)
+            {
+                std::cout << "Моё число МЕНЬШЕ!\n" << std::endl;
+            }
+            else if (guess < secretNumber)
+            {
+                std::cout << "Моё число БОЛЬШЕ!\n" << std::endl;
+            }
+        }
+        
+        std::cout << "Поздравляю! Ты угадал число " << secretNumber
+                  << " за " << attempts << " попыток!\n" << std::endl;
+        
+        std::cout << "Сыграть ещё раз? (1 - да, 0 - нет): ";
+        int answer;
+        std::cin >> answer;
+        playAgain = (answer == 1);
+        
+        std::cout << std::endl;
+    }
+    
+    std::cout << "Спасибо за игру! До встречи." << std::endl;
+    
     return 0;
 }
